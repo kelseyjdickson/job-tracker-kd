0887a810543412b978612620a20c0e201796278c
Kelsey Dickson <kelseymedved@gmail.com>
2023-10-11 10:56:25 -0400

    testing for deployment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Testing font</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi quisquam
+        quod eos ut ab. Aliquam, optio, doloremque exercitationem provident rem
+        ratione quia corrupti quos repellendus repudiandae, eum velit magni
+        reiciendis.
+      </p>
     </div>
   );
 }
